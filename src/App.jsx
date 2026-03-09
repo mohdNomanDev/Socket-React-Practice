67def1546df64ea0aac5800fe1a7bfc0fda8b600
Mohd Noman <mohdnoman0302@gmail.com>
2026-03-09 16:57:09 +0530

    setup created for socket.io

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+
+import "./App.css";
+import { socket } from "./socket.js";
+
+function App() {
+  useEffect(() => {
+    // socket.on('offersAvailable',(data)=>{
+    //   console.log(data);
+    // })
+    socket.on("offer", (message) => {
+      console.log(message);
+    });
+  });
+
+  return (
+    <>
+      <div>
+        <button
+          onClick={() => {
+            socket.emit("joinRoom", "madina", (confirmation) => {
+              console.log(confirmation);
+            });
+          }}
+        >
+          JoinRoom
+        </button>
+
+        <button
+          onClick={() => {
+            socket.emit("offer", {
+              room: "madina",
+              message: "We have a Offer",
+            });
+          }}
+        >
+          Send Message
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
